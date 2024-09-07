42bc052de8973f07cfe243a4e2da0902dd72ee4c
bara <bbd@gamil.com>
2024-09-07 00:44:41 +0000

    Message de commit

--- rapport_de_stage_back/app/Http/Controllers/Api/AuthController.php
+++ rapport_de_stage_back/app/Http/Controllers/Api/AuthController.php
@@ -4,8 +4,53 @@ namespace App\Http\Controllers\Api;
 use App\Models\User;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+use Tymon\JWTAuth\Facades\JWTAuth;
 class Authcontroller extends Controller
 {
    
- 
+    
+  
+    
+    public function login(Request $request)
+    {
+        $data =  $request->validate([
+            "email" => "required|email|",
+            "password" => "required"
+        ]);
+
+        $token = JWTAuth::attempt($data);
+
+        if(!empty($token))
+        {
+            return response()->json([
+                'statut' => 200,
+                "token" =>  $token
+            ]);
+
+        }else{
+            return response()->json([
+                "statut" => false,
+                "token" =>  null
+            ]);
+        }
+    }
+
+    public function logout()
+    {
+        auth()->logout();
+        return response()->json([
+            'statut' => true,
+            "message" =>  "user logout !"
+        ]);
+    }
+
+    public  function refresh()
+    {
+        $newToken = auth()->refresh();
+        return response()->json([
+            'statut' => true,
+            "token" =>  $newToken
+        ]);
+    }
 }
--- rapport_de_stage_back/routes/api.php
+++ rapport_de_stage_back/routes/api.php
@@ -17,3 +17,11 @@ use Illuminate\Support\Facades\Route;
 
 
 
+Route::post('/login', [\App\Http\Controllers\Api\AuthController::class, 'login'])->name('login');
+
+
+Route::middleware('auth:api')
+    ->group(function (){
+        Route::post('/logout',[\App\Http\Controllers\Api\AuthController::class,'logout'])->name('logout');        
+    });
+
